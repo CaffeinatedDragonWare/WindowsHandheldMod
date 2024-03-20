972bb14349d393c3dc70b4862634476eff1062fc
CaffeinatedDragonWare <71392168+CaffeinatedDragonWare@users.noreply.github.com>
2024-03-20 13:37:49 -0300

    Fixed error in steam_startup.bat

--- steam_startup.bat
+++ steam_startup.bat
@@ -6,7 +6,7 @@ start /min cmd /C "%~dpnx0"
 goto :EOF
 :Minimized
 
-start "" "C:\Program Files (x86)\Steam\Steam.exe" &
+start /B "" "C:\Program Files (x86)\Steam\Steam.exe" -noverifyfiles &
 start /B "" "%UserProfile%\Videos\ffplay.exe"  -left 0 -top 0 -x 1280 -y 800 -alwaysontop -fullscreen -noborder -autoexit -loglevel quiet -loop 1 "%UserProfile%\Videos\Boot.webm" 2>NUL &
 timeout /t 2
 start explorer.exe
