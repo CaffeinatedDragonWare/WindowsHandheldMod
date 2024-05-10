bf7fd124b43ecb9b9fe8e5042a2c73102514cf28
CaffeinatedDragonWare <71392168+CaffeinatedDragonWare@users.noreply.github.com>
2024-05-10 14:28:30 -0300

    Update startup.bat
    Added vbs support

--- startup.bat
+++ startup.bat
@@ -1,9 +1,14 @@
 @echo off
 
-if not "%Minimized%"=="" goto :Minimized
-set Minimized=True
-start /min cmd /C "%~dpnx0"
-goto :EOF
+if not exist "%UserProfile%\Videos\invisible_startup.vbs" (
+    if not "%Minimized%"=="" (
+        goto :Minimized
+    )
+    set Minimized=True
+    start /min cmd /C "%~dpnx0"
+    goto :EOF
+)
+
 :Minimized
 
 REM Replace YOUR LAUNCHER PATH with the path to the launcher of your choice
