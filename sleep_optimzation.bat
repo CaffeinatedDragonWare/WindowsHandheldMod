e6f2f66f40914a92bd0774ed6d0198a4194eff51
CaffeinatedDragonWare <71392168+CaffeinatedDragonWare@users.noreply.github.com>
2024-10-20 09:28:33 -0300

    Added optional sleep optimization script
    This makes windows handhelds act mimic the steam deck's sleep mode. Works best if you turn on auto sign in and remove your windows password.

---
+++ sleep_optimzation.bat
@@ -0,0 +1,3 @@
+powershell -Command "PowerCfg /SETACVALUEINDEX SCHEME_CURRENT SUB_NONE CONSOLELOCK 0"
+powershell -Command "PowerCfg /SETDCVALUEINDEX SCHEME_CURRENT SUB_NONE CONSOLELOCK 0"
+powershell -Command "PowerCfg -h off"
